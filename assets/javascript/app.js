bd355704276871f41de91dab3f08644651e93f03
catkins <catkinsjr28@gmail.com>
2017-06-18 17:01:10 -0400

    added Audio on click of start button.Counter displays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,42 @@
-var instructions;
+$( document ).ready(function() {
 
-function startInstructions() {
-    instructions = setInterval(function(){ alertFunc("First param", "Second param"); }, 2000);
-}
+var audio = new Audio("assets/audio/TarHeelsFightSong.mp3");
+
+
+var number = 120;
+
+    var intervalId;
+
+    $("#start").on("click", start);
+
+
+    function start() {
+      intervalId = setInterval(decrement, 1000);
+      audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+audio.play();
+    }
+
+    function decrement() {
+
+      number--;
+
+      $("#timeLeft").html("<h1>" + number + "</h1>");
+
+      if (number === 0) {
+
+        stop();
+
+        alert("Time Up!");
+      }
+    }
+
+    
+
+    //run();
+//120 seconds
+
+
+});
